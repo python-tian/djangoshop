5a5fe32484da4ed8de62bea0fd4835b66c00bac7
xiaotian <xiaotian@qq.com>
2019-07-22 12:06:35 +0800

    2019-07-22商城项目静态文件收集

--- Freshshop/Freshshop/settings.py
+++ Freshshop/Freshshop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Store',
+    'ckeditor',
+    'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
@@ -103,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
@@ -118,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
+MEDIA='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,"static")
+#STATIC_ROOT=os.path.join(BASE_DIR,"static")
+CKEDITOR_UPLOAD_PATH="static/upload"
+CKEDITOR_IMAGE_BACGEND='pillow'
+
+
